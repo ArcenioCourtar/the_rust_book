4b5bde14a1ed55ec8b4dcc64065f951cddf2f409
Arcenio Courtar <71286201+ArcenioCourtar@users.noreply.github.com>
2025-07-20 03:12:49 +0200

    temp converter

---
+++ ch3/ex_temp_converter/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+
+fn main() {
+    let mut temp = String::new();
+    println!("Please pass Fahrenheit temp");
+    io::stdin()
+        .read_line(&mut temp)
+        .expect("Failed to read line");
+
+    let temp: i32 = match temp.trim().parse() {
+        Ok(temp) => temp,
+        Err(_) => {
+            println!("NUMBER PLS");
+            std::process::exit(0);
+        }
+    };
+
+    if temp < -459 {
+        println!("A bit too cold chief");
+        std::process::exit(0);
+    }
+    
+    println!("temperature in Fahrenheit: {temp}");
+    println!("temperature in Celsius: {}", (temp - 32) * 5 / 9);
+
+    // another way of getting Celsius. More shadowing!
+    let temp = (temp - 32) * 5 / 9;
+    println!("temperature in Celsius: {temp}");
+}
